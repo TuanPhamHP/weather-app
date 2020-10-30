8d227c843379d56efae931d1ffaa1742831d5b3a
TuanPhamHP <phamanhtuan290896@gmail.com>
2020-10-30 15:36:03 +0700

    deploy my weatherapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,134 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect, useCallback } from "react";
+import Axios from "axios";
+import WeatherCard from "./components/WeatherCard";
+import "./assets/style/main.scss";
+const KEY = "f5ff5f05cff9a15fb369cecc24fe826c";
+const initialWeather = {
+  coord: {
+    lon: 107.83,
+    lat: 16.17,
+  },
+  weather: [
+    {
+      id: 802,
+      main: "Clouds",
+      description: "scattered clouds",
+      icon: "03d",
+    },
+  ],
+  base: "stations",
+  main: {
+    temp: 303.64,
+    feels_like: 308.79,
+    temp_min: 302.15,
+    temp_max: 305.15,
+    pressure: 1008,
+    humidity: 74,
+  },
+  visibility: 10000,
+  wind: {
+    speed: 2.1,
+    deg: 200,
+  },
+  clouds: {
+    all: 40,
+  },
+  dt: 1592363664,
+  sys: {
+    type: 1,
+    id: 9310,
+    country: "VN",
+    sunrise: 1592345821,
+    sunset: 1592392935,
+  },
+  timezone: 25200,
+  id: 1562822,
+  name: "Vietnam",
+  cod: 200,
+};
 function App() {
+  const [weather, setWeather] = useState(initialWeather);
+  const [weatherHly, setWeatherHly] = useState(null);
+  const [query, setQuery] = useState([]);
+  const [search, setSearch] = useState([]);
+  const [hourly, setHourly] = useState(
+    JSON.parse(localStorage.getItem("initValue"))
+      ? JSON.parse(localStorage.getItem("initValue")).coord
+      : initialWeather.coord
+  );
+  const reg = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${KEY}`;
+  const regH = `https://api.openweathermap.org/data/2.5/onecall?lat=${hourly.lat}&lon=${hourly.lon}&exclude=minutely,daily&appid=${KEY}`;
+  const handleSearch = useCallback(async () => {
+    try {
+      const result = await Axios.get(reg);
+      setWeather(result.data);
+      setHourly(result.data.coord);
+    } catch (error) {
+      console.log(error);
+      setWeather(
+        JSON.parse(localStorage.getItem("initValue"))
+          ? JSON.parse(localStorage.getItem("initValue"))
+          : initialWeather
+      );
+      setHourly(
+        JSON.parse(localStorage.getItem("initValue"))
+          ? JSON.parse(localStorage.getItem("initValue")).coord
+          : initialWeather.coord
+      );
+    }
+  }, [regH, hourly, reg]);
+  useEffect(() => {
+    handleSearch();
+  }, [query]);
+
+  useEffect(() => {
+    try {
+      const resultH = Axios.get(regH);
+      resultH.then((respond) => {
+        setWeatherHly(respond.data.hourly);
+        console.log(respond.data.hourly);
+      });
+    } catch (err) {
+      const resultH = Axios.get(regH);
+      resultH.then((respond) => setWeatherHly(respond.data.hourly));
+    }
+  }, [hourly]);
+
+  const handleUpdate = (e) => {
+    let string = e.target.value;
+    setSearch(string);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let x = search;
+    setQuery(search);
+    setSearch("");
+  };
+  const getWeather = () => {
+    switch (weather.weather[0].main) {
+      case "Rain":
+        return "rainny-day";
+      case "Clouds":
+        return "cloudy-day";
+      case "Clear":
+        return "clear-sky";
+      case "Smoke":
+        return "smoke-day";
+      default:
+        return "";
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${getWeather()}`}>
+      <form action="submit" onSubmit={handleSubmit} className="search-form">
+        <input
+          type="text"
+          placeholder="search here"
+          onChange={handleUpdate}
+          value={search}
+        />
+      </form>
+      <WeatherCard weather={weather} weatherHly={weatherHly} />
     </div>
   );
 }
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,139 @@
+import React, { useState, useEffect } from "react";
+
+function formatDate(date, gmt) {
+  const hours =
+    date.getUTCHours() + gmt < 0 ? `${date.getUTCHours() + gmt + 24}` : date.getUTCHours() + gmt;
+  const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+  const seconds = date.getSeconds();
+  return `${hours} : ${minutes}`;
+}
+
+function WeatherCard({ weather, weatherHly }) {
+  const t = new Date();
+  const [time, setTime] = useState(formatDate(t));
+  const [weatherHrly, setWeatherHrly] = useState(weatherHly);
+  useEffect(() => {
+    const date = new Date();
+    formatDate(date, weather.timezone / 3600);
+    setTimeout(() => {
+      const now = new Date();
+      let newTime = formatDate(now, +(weather.timezone / 3600));
+      setTime(newTime);
+      setWeatherHrly(weatherHly);
+    }, 1);
+    const timeInterval = setInterval(() => {
+      const now = new Date();
+      let newTime = formatDate(now, +(weather.timezone / 3600));
+      setTime(newTime);
+    }, 6000);
+    return () => {
+      clearInterval(timeInterval);
+    };
+  });
+  const getWeather = () => {
+    switch (weather.weather[0].main) {
+      case "Rain":
+        return "rainny-day";
+      case "Clouds":
+        return "cloudy-day";
+      case "Clear":
+        return "clear-sky";
+      case "Smoke":
+        return "smoke-day";
+      default:
+        return "";
+    }
+  };
+  const getTemp = () => {
+    if (weather.main.temp < 292) {
+      return "cold-day";
+    } else if (292 < weather.main.temp && weather.main.temp < 300) {
+      return "fresh-day";
+    } else if (weather.main.temp > 300) {
+      return "hot-day";
+    }
+  };
+  const setDefaultWeather = () => {
+    localStorage.clear();
+    localStorage.setItem("initValue", JSON.stringify(weather));
+  };
+  const getHourlyTemp = () => {
+    if (!weatherHrly) {
+      return;
+    }
+    // console.log(weatherHrly);
+    const parseToDate = mls => {
+      let ct = new Date(mls * 1000);
+      let ch = `${JSON.stringify(ct.getHours())} : 00`;
+      return <p className='time'>{ch}</p>;
+    };
+    const toDay = weatherHrly.filter(item => {
+      if (
+        new Date(item.dt * 1000).getHours() > new Date().getHours() &&
+        new Date(item.dt * 1000).getHours() < 24
+      ) {
+        return item;
+      }
+    });
+    const hourlyTemp = toDay.map(item => {
+      return (
+        <div key={item.dt} className='hourly-items'>
+          {parseToDate(item.dt)}
+          <p className='temp'>
+            {(item.temp - 273.15).toFixed(1)}
+            <sup>°C</sup>
+          </p>
+          <p className='description'>{item.weather[0].description}</p>
+        </div>
+      );
+    });
+    return hourlyTemp;
+  };
+
+  return (
+    <div>
+      <div
+        key={Math.random() * Date.now()}
+        className={`weather-card main-card ${getWeather()} ${getTemp()}`}
+      >
+        <button className='set-default' onClick={setDefaultWeather}>
+          Set as default
+        </button>
+        <p className='weather-name'>{weather.name}</p>
+        <p className='weather-temp'>
+          {(weather.main.temp - 273.15).toFixed(1)}
+          <sup>°C</sup>
+        </p>
+        <div className='weather-temp--minmax'>
+          {" "}
+          <p className='weather-temp--min'>
+            Min :{(weather.main.temp_min - 273.15).toFixed(1)}
+            <sup>°C</sup>
+          </p>
+          <p className='weather-temp--max'>
+            Max :{(weather.main.temp_max - 273.15).toFixed(1)}
+            <sup>°C</sup>
+          </p>
+        </div>
+        <p className='weather-description'>{weather.weather[0].description}</p>
+        <p className='weather-humidity'>Humidity :{weather.main.humidity}%</p>
+        <p className='notice notice-cold'>
+          Beware, my friends to day is quite cold . Keep your self warm .
+        </p>
+        <p className='notice notice-cool'>
+          What a nice day. My friends, to day is a good day . Keep your self up .
+        </p>
+        <p className='notice notice-hot'>
+          Beware, my friends to day is a hot day . Keep your self calm and have a good day .
+        </p>
+        <div className='hourly-wrapper'>{getHourlyTemp()}</div>
+        <p className='weather-time'>{time}</p>
+        <p className='weather-GMT'>
+          Time Zone : GMT
+          {weather.timezone < 0 ? `${weather.timezone / 3600}` : `+${weather.timezone / 3600}`}
+        </p>
+      </div>
+    </div>
+  );
+}
+export default WeatherCard;
